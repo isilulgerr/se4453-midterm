3df9c8432c2197046af6848c516223adb8c4b4a2
Işıl <isilulgerr@gmail.com>
2025-04-17 00:34:41 +0300

    Add PostgreSQL KeyVault connection

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask
+import psycopg2
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 app = Flask(__name__)
+
+# Azure Key Vault bağlantısı
+VAULT_URL = "https://hellovaultgr1.vault.azure.net/"
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=VAULT_URL, credential=credential)
+
+# PostgreSQL bilgilerimizi vault'tan çekiyoruz
+PG_HOST = client.get_secret("POSTGRES-HOST").value
+PG_USERNAME = client.get_secret("POSTGRES-USERNAME").value
+PG_PASSWORD = client.get_secret("POSTGRES-PASSWORD").value
+PG_DB = client.get_secret("POSTGRES-DATABASE").value
 
 @app.route("/")
 def home():
     return "Hello from Azure App Service 🚀"
 
 @app.route("/db")
-def db_check():
-    return "PostgreSQL test endpoint 🎯"
+def db():
+    try:
+        conn = psycopg2.connect(
+            host=PG_HOST,
+            database=PG_DB,
+            user=PG_USERNAME,
+            password=PG_PASSWORD
+        )
+        conn.close()
+        return "PostgreSQL test endpoint 🎯"
+    except Exception as e:
+        return f"Database error: {str(e)}"
 
 if __name__ == "__main__":
     app.run()
