ef3976202daf8a8c42acee6ef20d7de69203870d
Işıl <isilulgerr@gmail.com>
2025-04-17 00:28:12 +0300

    Added /db test endpoint

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello from Azure App Service 🚀"
 
+@app.route("/db")
+def db_check():
+    return "PostgreSQL test endpoint 🎯"
+
 if __name__ == "__main__":
     app.run()
