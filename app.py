ff0096e113e3124771c71648c9d2cc14e8c31acf
Işıl <isilulgerr@gmail.com>
2025-06-05 19:23:42 +0300

    Add try-catch logging for DB connection

--- app.py
+++ app.py
@@ -6,16 +6,20 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    conn = psycopg2.connect(
-        host=os.environ.get("POSTGRES_HOST"),
-        dbname=os.environ.get("POSTGRES_DATABASE"),
-        user=os.environ.get("POSTGRES_USERNAME"),
-        password=os.environ.get("POSTGRES_PASSWORD")
-    )
-    cur = conn.cursor()
-    cur.execute("SELECT 'Hello from DB!'")
-    result = cur.fetchone()[0]
-    return result
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get("POSTGRES_HOST"),
+            dbname=os.environ.get("POSTGRES_DATABASE"),
+            user=os.environ.get("POSTGRES_USERNAME"),
+            password=os.environ.get("POSTGRES_PASSWORD")
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT 'Hello from DB!'")
+        result = cur.fetchone()[0]
+        return result
+    except Exception as e:
+        return f"Connection failed: {str(e)}"
+
 
 if __name__ == "__main__":
     app.run()
