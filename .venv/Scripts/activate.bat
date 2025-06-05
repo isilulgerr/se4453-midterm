c0b3ae4b3319a0b8d9574c78a8ce5efd581623d3
Işıl <isilulgerr@gmail.com>
2025-06-05 17:35:37 +0300

    Initial commit before git flow init

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\isilu\OneDrive\Masaüstü\SE4453-Midterm Project\.venv
+set VIRTUAL_ENV=C:\Users\isilu\OneDrive\Masaüstü\SE4453-Midterm Project\SE4453-Midterm Project\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
